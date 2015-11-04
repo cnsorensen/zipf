024f949106d508615269324d83530b15980ab4a6
lukas783 <lucas.carpenter@mines.sdsmt.edu>
2015-11-03 19:45:11 -0700

    changed zipf.cpp

--- zipf.cpp
+++ zipf.cpp
@@ -1,13 +1,67 @@
+/**
+ * Includes
+**/
 #include <iostream>
-#include "hashTable.h"
+#include <fstream>
+#include <string>
+#include <algorithm>
+//#include "hashTable.h"
 
 using namespace std;
 
-int main()
+
+/**
+ * Prototypes
+**/
+void fileHandle(char* fileName);
+string tokenize(string str);
+
+
+/**
+ * Functions
+**/
+
+int main(int argc, char*argv[])
 {
-    hashTable T;
+  switch(argc) {
+    //if 2 args, pass to file handling and continue program
+    case 2:
+      fileHandle(argv[1]);
+    break;
 
-    cout << "hello world" << endl;
+    //if more or less than 2 args print out usage statement
+    default: 
+    cout << "Usage: zipf filename\n";
+    return 1;
+  }
+  
+  //hashTable T;
+  
+  return 0;
+}
 
-    return 0;
+void fileHandle(char* fileName) {
+  ifstream in;
+  in.open(fileName, ios::in);
+
+  string x;
+  
+  while(in >> x) {
+    x = tokenize(x);
+    cout << x << " ";
+  }
+  
+}
+
+/** 
+ * not done 
+**/
+string tokenize(string str) {
+  transform(str.begin(), str.end(), str.begin(), ::tolower);
+  for(int i = 0;i<str.length();i++) {
+    if(str[i] == 
+  }
+  
+  return str;
+  
 }
