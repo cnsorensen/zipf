f6a22eecb329dbb87a7580c6f420ce425f7c69ca
lukas783 <lucas.carpenter@mines.sdsmt.edu>
2015-11-05 16:14:37 -0700

    Added tokenize function to zipf.cpp

--- zipf.cpp
+++ zipf.cpp
@@ -9,6 +9,10 @@
 
 using namespace std;
 
+/**
+ * Global Variables
+**/
+const char* valid = "abcdefghijklmnopqrstuvwxyz'";
 
 /**
  * Prototypes
@@ -16,11 +20,15 @@ using namespace std;
 void fileHandle(char* fileName);
 string tokenize(string str);
 
-
 /**
  * Functions
 **/
 
+/**
+ * main, the starting point of our program, reads in command
+ * line args and passes them off to other functions or prints
+ * out a usage statement
+**/
 int main(int argc, char*argv[])
 {
   switch(argc) {
@@ -34,15 +42,20 @@ int main(int argc, char*argv[])
     cout << "Usage: zipf filename\n";
     return 1;
   }
-  
-  //hashTable T;
-  
   return 0;
 }
 
+/**
+ * fileHandle, reads in a file and passes each word of the file
+ * onto a tokenize function and inserts the word into a hashtable
+**/
 void fileHandle(char* fileName) {
   ifstream in;
   in.open(fileName, ios::in);
+  if (!in) {
+	  cout << "Unable to open " << fileName << ". Exiting program.\n";
+	  return;
+  }
 
   string x;
   
@@ -54,14 +67,34 @@ void fileHandle(char* fileName) {
 }
 
 /** 
- * not done 
+ * tokenize, takes in an input string and makes all chars lowercase, then
+ * checks for valid chars throughout the string and returns out the newly
+ * modified string. This function is a modification of Dr. Weiss's tokenize2
+ * code.
 **/
 string tokenize(string str) {
-  transform(str.begin(), str.end(), str.begin(), ::tolower);
-  for(int i = 0;i<str.length();i++) {
-    if(str[i] == 
-  }
-  
-  return str;
-  
+	// this will lowercase the string
+	transform(str.begin(), str.end(), str.begin(), ::tolower);
+	
+	// skip delimiters to start of first token
+	int tokenStart = str.find_first_of(valid, 0);
+	// find next delimiter (i.e., end of first token)
+	int tokenEnd = str.find_first_not_of(valid, tokenStart);
+
+	// since tokenEnd will return a -1 if no invalid char is found
+	// we need to change it to the length of the string
+	if (tokenEnd == -1) {
+		tokenEnd = str.size();
+	}
+	// remove beginning ' or end ' 
+	if (str[tokenEnd-1] == '\'')
+		tokenEnd--;
+	if (str[tokenStart] == '\'')
+		tokenStart++;
+
+	// substring our word from tokenStart to tokenEnd
+	str = str.substr(tokenStart, (tokenEnd - tokenStart));
+
+	//output our newly modified string
+	return str;
 }
