3cba7db781b95a250f99c70c7ab310674f186c6d
cnsorensen <christine.sorensen@mines.sdsmt.edu>
2015-11-08 17:22:48 -0700

    The beginning of the hash table. I have it create a table, delete table, insert word, find word, as well as create an int value for the string. It compiles, I haven't tested for functionality yet. Also, some of the features haven't been implemented like checking if the table is 75% full yet, looping through the table when finding the item, and stopping if it doesn't find it.

--- hashTable.h
+++ hashTable.h
@@ -1,17 +1,47 @@
 /* hashTable.h */
 
+#ifndef _HashTable_H_
+#define _HashTable_H_
+
+#include <string>
+using namespace std;
+
 class hashTable
 {
     public:
         // constructor
-        hashTable();
+        hashTable( int tableSize = 0, int tableFilled = 0 );
+        
+        // copy constructor
+        hashTable( const hashTable& );
+
         // destructor
         ~hashTable();
     
-        //insert
-        //delete
-        //find
+        // insert
+        bool insertItem( const string& );
+        
+        // delete
+        bool deleteItem( const string& );
+
+        // find
+        int findItem( const string& );
+
+
+        friend int stringToInt( const string& );
+
+        struct hashItem
+        {
+            string word;
+            int freq;       
+        };
+
 
     private:
         int tableSize;
+        int tableFilled;
+        hashItem* table;
 };
+
+
+#endif
