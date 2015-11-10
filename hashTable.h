3034c5ad0cca8bd7e540cce2e5392ae79da21dbf
cnsorensen <christine.sorensen@mines.sdsmt.edu>
2015-11-09 20:27:58 -0700

    my hashing

---
+++ hashTable.h
@@ -0,0 +1,57 @@
+/* hashTable.h */
+//guard against multiple inclusions
+#ifndef __HASHTABLE_H_
+#define __HASHTABLE_H_
+
+// inlcudes
+#include <string>
+#include <iostream>
+#include <fstream>
+
+//class declaration
+class hashTable
+{
+    public:
+        // constructor
+        hashTable();
+        // destructor
+        ~hashTable();
+    
+	void resize(int newSize);
+	bool insert(std::string s);
+	void sort();
+	void printStats(std::string file);
+	int getWords();
+	int getDistinct();
+	int getSize();
+        //delete
+        //find
+	
+	struct tableItem {
+	  std::string word = "";
+	  int freq = -1;
+	};
+
+        //friend int compare( const void*, const void* );
+
+    private:
+		/**
+		 * maybe if we store it as a struct, we can make an array of structs
+		 * and access each index using a key.. like with a starting size of 1009
+		 * the key would be  (word[0] * (size/26)), if we have a large array of 
+		 * these words we can enhance the key to be (word[0] * (size/26) + word[1]
+		 * or something similar, not sure yet.. will have to evaluate more, but the
+		 * key can be upgraded later to see if more efficient.
+		**/
+		
+		tableItem *table = nullptr;
+        int tableSize = 0, distinct = 0, words = 0;
+		int fullSize = 1352;
+<<<<<<< HEAD
+        static int compareFreq( const void*, const void* );
+=======
+	        //int comp(tableItem *p1, tableitem *p2);
+>>>>>>> 8ce893cb1e03cfdbe008cbf3b369c387797d5477
+};
+
+#endif
